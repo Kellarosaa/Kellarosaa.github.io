e1ade04a484bfc2bc2d160557460a11c4ea1eb95
Kellarosaa <146978293+Kellarosaa@users.noreply.github.com>
2024-05-05 02:41:57 -0500

    Create checkers.js

---
+++ Checkers/checkers.js
@@ -0,0 +1,29 @@
+const board = [
+  [0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
+  [-1, 0, -1, 0, -1, 0, -1, 0, -1, 0],
+  [0, -1, 0, -1, 0, -1, 0, -1, 0, -1],
+  [-1, 0, -1, 0, -1, 0, -1, 0, -1, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1],
+  [1, 0, 1, 0, 1, 0, 1, 0, 1, 0],
+  [0, 1, 0, 1, 0, 1, 0, 1, 0, 1]
+];
+
+const game = document.getElementById("game");
+const blackPiecesCounter = document.getElementById("black-player-count-pieces");
+const whitePiecesCounter = document.getElementById("white-player-count-pieces");
+
+let currentPlayer = -1; // -1 for black, 1 for white
+
+function buildBoard() {
+  game.innerHTML = "";
+
+  for (let i = 0; i < board.length; i++) {
+    const element = board[i];
+    let row = document.createElement("div"); // create div for each row
+    row.setAttribute("class", "row");
+
+    for (let j = 0; j < element.length; j++) {
+      const elmt = element
